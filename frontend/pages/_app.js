001034bba769dd6dc04849c57008be81a0ece2a4
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-05-23 21:51:25 +0530

    Add files via upload

---
+++ frontend/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import '../styles/slide.scss'
+import '../styles/variable.scss'
+import '../public/css/bootstrap.css'
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <Component {...pageProps} />
+  );
+  
+  
+}
+
+export default MyApp
