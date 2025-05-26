ea43a6365e28f16787a41cd76976f62d332c3b30
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-05-26 22:23:58 +0530

    Update studentAction.js

--- frontend/actions/studentAction.js
+++ frontend/actions/studentAction.js
@@ -17,3 +17,69 @@ export const student_list = () => {
         })
         .catch(err => console.log(err));
 };
+
+export const add_student = patient => {
+    return fetch(`${API}/add-student`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(patient)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+export const student_list_byid = (student_id) => {
+    // alert(guardian_id)
+    let data = {
+        'std_id' :student_id
+    };
+    return fetch(`${API}/student_list_byid`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+export const edit_student = std_id => {
+    return fetch(`${API}/student_update`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(std_id)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const DeleteStd = (query) => {
+    return fetch(`${API}/student_delete`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(query)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
