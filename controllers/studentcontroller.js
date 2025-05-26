c449371eff25f1f89fa0034ecdeeab0f903b5fb2
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-05-26 22:30:37 +0530

    Update studentcontroller.js

--- controllers/studentcontroller.js
+++ controllers/studentcontroller.js
@@ -38,17 +38,41 @@ exports.student_list = function (req, res) {
     });
 };
 
+exports.student_list_byid = function (req, res) {
+    var list = []
+    Student.find({ deleted_status: false,_id:req.body.std_id }, function (err, resp) {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      } else {
+        for (var i = 0; i < resp.length; i++) {
+          list.push({
+            sno: i + 1,
+            _id: resp[i]._id,
+            student_name: resp[i].student_name,
+            student_email: resp[i].student_email,
+            student_usn: resp[i].student_usn,
+            student_branch:resp[i].student_branch,
+          })
+        }
+        res.json({ student_list: list, error: false });
+      }
+    });
+  };
 
  exports.student_update=async function(req,res){
     if (!req.body.std_id) {
         return res.status(400).json({
-            error: 'Missing admin user id'
+            error: 'Missing student user id'
         });
     } else {
         var query = {
             student_name: req.body.student_name,
             student_email:req.body.student_email,
             student_usn:req.body.student_usn,
+            student_branch:req.body.student_branch,
+
         }
         await Student.findOneAndUpdate({ _id: req.body.std_id }, query);
         res.json({
@@ -57,4 +81,35 @@ exports.student_list = function (req, res) {
     }
 }
 
- 
+
+
+exports.student_delete= async function(req,res){
+    if (!req.body.std_id) {
+        return res.status(400).json({
+            error: 'Missing student id'
+        });
+    } 
+    try{
+        const result= await student.findOneAndUpdate(
+            {_id:req.body.std_id },
+            {deleted_status:true},
+            {new:true}
+        );
+        if(!result){
+            return res.status(404).json({
+                error:'Student not found'
+            });
+        }
+        res.json({
+            message:'Deleted Successfully',
+        });       
+    }
+    catch(err){
+        res.status(500).json({
+            error:'Error deleting student',
+            details: err.message
+        })
+
+    }
+    
+};  
