c565a5a292192b687fe0777e1b714dd17015ea9c
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-05-31 00:00:41 +0530

    Update studentRouter.js

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -5,7 +5,7 @@ const studentController = require('../controllers/studentcontroller');
 router.post('/add-student', studentController.add_student);
 router.get('/list-students', studentController.student_list);
 router.post('/student_update', studentController.student_update);
-router.get('/student_list_byid', studentController.student_list_byid);
+router.post('/student_list_byid', studentController.student_list_byid);
 router.post('/student_delete', studentController.student_delete);
 
 module.exports = router;
