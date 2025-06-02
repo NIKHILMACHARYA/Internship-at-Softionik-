50561605a86951e74bcfb77d49c6fbeb3a79a13e
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-03 00:27:29 +0530

    Update stateRouter.js

--- routes/stateRouter.js
+++ routes/stateRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const stateController = require('../controllers/stateController.js');
+const stateController = require('../controllers/statecontroller.js');
 
-router.get('/list-state', stateController.state_list);
+router.get('/state_list', stateController.state_list);
 router.post('/add_state', stateController.add_state);
+router.post('/state_update', stateController.state_update);
+router.post('/state_list_byid', stateController.state_list_byid);
+router.post('/state_delete', stateController.state_delete);
 
 module.exports = router;
