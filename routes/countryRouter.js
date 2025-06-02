b370db8b1bee3befa769f9c340487d8e3851b0a7
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-03 00:26:52 +0530

    Update countryRouter.js

--- routes/countryRouter.js
+++ routes/countryRouter.js
@@ -4,5 +4,8 @@ const countrycontroller = require('../controllers/countrycontroller.js');
 
 router.post('/add_country', countrycontroller.add_country);
 router.get('/country_list', countrycontroller.country_list);
- 
+router.post('/country_list_byid', countrycontroller.country_list_byid);
+router.post('/country_update', countrycontroller.country_update);
+router.post('/country_delete', countrycontroller.country_delete);
+
 module.exports = router;
