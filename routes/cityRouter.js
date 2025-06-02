d4815f1c1c6a1020b2ff4893bcea0599f78ee885
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-03 00:26:15 +0530

    Update cityRouter.js

--- routes/cityRouter.js
+++ routes/cityRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const cityController = require('../controllers/cityController.js');
+const citycontroller = require('../controllers/citycontroller.js');
 
-router.get('/city-list', cityController.city_list);
-router.post('/add_city', cityController.add_city);
+router.get('/city_list', citycontroller.city_list);
+router.post('/add_city', citycontroller.add_city);
+router.post('/city_list_byid', citycontroller.city_list_byid);
+router.post('/city_update', citycontroller.city_update);
+router.post('/city_delete', citycontroller.city_delete);
 
 module.exports = router;
