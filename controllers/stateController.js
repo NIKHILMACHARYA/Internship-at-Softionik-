6015ccb6c740afa54f04b18d72d6758a180fa918
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-03 00:21:25 +0530

    Update stateController.js

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -1,8 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
+const moment = require('moment');
 const state = mongoose.model('state');
 const country = mongoose.model('country');
-var moment = require('moment');
 
 exports.add_state = function (req, res) {
     const body = req.body;
@@ -22,7 +22,6 @@ exports.add_state = function (req, res) {
 
 exports.state_list = async function (req, res) {
     var list = [];
-
     state.find({ deleted_status: false }, async function (err, resp) {
         if (err) {
             return res.status(400).json({
@@ -52,3 +51,84 @@ exports.state_list = async function (req, res) {
         }
     });
 };
+
+
+exports.state_list_byid = async function (req, res) {
+    var list = []
+    state.find({ deleted_status: false,_id:req.body.st_id }, async function (err, resp) {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      } else {
+        for (var i = 0; i < resp.length; i++) {
+            // if (mongoose.Types.ObjectId.isValid(resp[i].country_id)) 
+               var countryArr = await country.find({ _id: resp[i].country_id }).exec();
+               var country_name = "";
+               if (countryArr[0] != undefined && countryArr[0] != "") {
+                   country_name = countryArr[0].country_name;
+                }
+           
+
+          list.push({
+            sno: i + 1,
+            _id: resp[i]._id,
+            state_name: resp[i].state_name,
+            country_id: resp[i].country_id,
+            country_name: country_name
+          })
+        }
+        res.json({ state_list: list, error: false });
+      }
+    });
+  };
+
+
+ exports.state_update=async function(req,res){
+    if (!req.body.st_id) {
+        return res.status(400).json({
+            error: 'Missing state id'
+        });
+    } else {
+        var query = {
+            state_name: req.body.state_name,
+            country_id:req.body.country_id,
+            
+        }
+        await state.findOneAndUpdate({ _id: req.body.st_id }, query);
+        res.json({
+            message: "Updated successfully"
+        });
+    }
+}
+
+
+
+exports.state_delete= async function(req,res){
+    if (!req.body.st_id) {
+        return res.status(400).json({
+            error: 'Missing state id'
+        });
+    } 
+    try{
+        const result= await state.findOneAndUpdate(
+            {_id:req.body.st_id },
+            {deleted_status:true},
+            {new:true}
+        );
+        if(!result){
+            return res.status(404).json({
+                error:'State not found'
+            });
+        }
+        res.json({
+            message:'Deleted Successfully',
+        });       
+    }
+    catch(err){
+        res.status(500).json({
+            error:'Error deleting state',
+            details: err.message
+        })
+    }
+};  
