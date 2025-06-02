0b27592faeb2b56b2ec90bc17700710655803016
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-03 00:22:46 +0530

    Update countrycontroller.js

--- controllers/countrycontroller.js
+++ controllers/countrycontroller.js
@@ -18,7 +18,6 @@ exports.add_country = function (req, res) {
     });
 };
 
-
 exports.country_list = function (req, res) {
     country.find({ deleted_status: false }, function (err, country) {
         if (err) return res.status(400).json({ error: err });
@@ -32,3 +31,76 @@ exports.country_list = function (req, res) {
         res.json({ country_list: list, error: false });
     });
 };
+
+
+exports.country_list_byid = function (req, res) {
+    var list = []
+    country.find({ deleted_status: false,_id:req.body.cntry_id }, function (err, resp) {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      } else {
+        for (var i = 0; i < resp.length; i++) {
+          list.push({
+            sno: i + 1,
+            _id: resp[i]._id,
+            country_name: resp[i].country_name
+          })
+        }
+        res.json({ country_list: list, error: false });
+      }
+    });
+  };
+
+
+
+ exports.country_update=async function(req,res){
+    if (!req.body.cntry_id) {
+        return res.status(400).json({
+            error: 'Missing country id'
+        });
+    } else {
+        var query = {
+            country_name:req.body.country_name
+
+        }
+        await country.findOneAndUpdate({ _id: req.body.cntry_id }, query);
+        res.json({
+            message: "Updated successfully"
+        });
+    }
+}
+
+
+
+exports.country_delete= async function(req,res){
+    if (!req.body.cntry_id) {
+        return res.status(400).json({
+            error: 'Missing country id'
+        });
+    } 
+    try{
+        const result= await country.findOneAndUpdate(
+            {_id:req.body.cntry_id },
+            {deleted_status:true},
+            {new:true}
+        );
+        if(!result){
+            return res.status(404).json({
+                error:'Country not found'
+            });
+        }
+        res.json({
+            message:'Deleted Successfully',
+        });       
+    }
+    catch(err){
+        res.status(500).json({
+            error:'Error deleting country',
+            details: err.message
+        })
+
+    }
+    
+};  
