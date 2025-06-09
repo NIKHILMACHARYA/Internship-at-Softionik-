459ab3cb9fdd7cbb534e891a118cd0b9f295ec3d
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-09 22:16:52 +0530

    Update imageRouter.js

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -5,5 +5,8 @@ const imageuploadController = require('../controllers/imageuploadController.js')
 
 router.post('/add_image', imageuploadController.single('img'),imageController.add_image );
 router.post('/edit_image', imageuploadController.single('img'),imageController.edit_image );
+router.get('/list_image', imageuploadController.single('img'),imageController.list_image );
+router.post('/list_byid_image', imageuploadController.single('img'),imageController.list_byid_image );
+router.post('/delete_image', imageuploadController.single('img'),imageController.delete_image );
 
 module.exports = router;
