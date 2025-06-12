167f68a46bb1d681b34a0830df4731a1fcb75127
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-12 21:27:47 +0530

    Update adminRouter.js

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,5 +4,6 @@ const adminController = require('../controllers/adminController.js');
 
 router.post('/login', adminController.login);
 router.post('/add_user', adminController.add_user);
+router.get('/admin_list', adminController.admin_list);
 
 module.exports = router;
